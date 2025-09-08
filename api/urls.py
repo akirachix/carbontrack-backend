febb1ecbc81b76c8b4bd3111788277c40934c076
paulathejennifer <paulathejennifer@gmail.com>
2025-09-08 20:05:53 +0300

    resolved conflicts

--- api/urls.py
+++ api/urls.py
@@ -1,40 +1,14 @@
-from django.urls import path,include
-from rest_framework.routers import DefaultRouter
-from .views import EmissionsViewSet
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-
-from rest_framework.routers import DefaultRouter
-from .views import FactoryViewSet, MCUViewSet
-from .views import EnergyEntryViewSet
-
-
+from .views import FactoryViewSet, MCUViewSet, EnergyEntryViewSet, EmissionsViewSet, ComplianceViewSet
 
 router = DefaultRouter()
 router.register(r'emissions', EmissionsViewSet, basename='emissions')   
 router.register(r'factories', FactoryViewSet)
 router.register(r'mcus', MCUViewSet)
 router.register(r'energy_entries', EnergyEntryViewSet)
-
-
-urlpatterns = [
-    path('api/', include(router.urls)),
-    path('api/factories/',FactoryViewSet.as_view, name='factory-api'),
-    path('api/mcus/',FactoryViewSet.as_view, name='factory-api'),
-
-    ]
-
-
-
-
-from rest_framework.routers import DefaultRouter
-from api.views import ComplianceViewSet
-
-
-router = DefaultRouter()
 router.register(r'compliance', ComplianceViewSet)
 
-
-urlpatterns=[
+urlpatterns = [
     path('api/', include(router.urls)),
 ]
