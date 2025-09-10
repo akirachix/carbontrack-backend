ccbdeb763bd130ef122009cc14a661a2c617f1d2
BerissaMuyizere <muyizereberissa@gmail.com>
2025-09-10 08:34:39 +0300

    created cicd for deployment, added deployment requirements

--- carbon_track/settings.py
+++ carbon_track/settings.py
@@ -15,8 +15,8 @@ import os
 from dotenv import load_dotenv
 from pathlib import Path
 import dj_database_url
-from decouple import config
 
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -75,10 +75,10 @@ REST_FRAMEWORK = {
 }
 
 MIDDLEWARE = [
-    
+
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
