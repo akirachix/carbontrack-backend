815f8fd4e243382dc6bd178ae5c34fdf76f96119
BerissaMuyizere <muyizereberissa@gmail.com>
2025-09-10 08:12:08 +0300

    created cicd for deployment, added deployment requirements

--- carbon_track/settings.py
+++ carbon_track/settings.py
@@ -14,6 +14,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from dotenv import load_dotenv
 from pathlib import Path
+import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
