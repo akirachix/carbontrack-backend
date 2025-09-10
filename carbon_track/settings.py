4cbfea4b6c945e524ce76ae859e954ebedd6cbed
BerissaMuyizere <muyizereberissa@gmail.com>
2025-09-10 08:26:26 +0300

    created cicd for deployment, added deployment requirements

--- carbon_track/settings.py
+++ carbon_track/settings.py
@@ -15,6 +15,7 @@ import os
 from dotenv import load_dotenv
 from pathlib import Path
 import dj_database_url
+from decouple import config
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
